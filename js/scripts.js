0f541713468fd56378d49ab6127767cf3c0346a3
Richie Zhang <richiezhang256@gmail.com>
2016-12-05 10:52:28 -0800

    Fix alignment issue with navbar color change

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 // Main Javascript file
 
-
+// Make navbar change color when scrolling past the cover page.
 $(window).scroll(function() {
-    if ($(window).scrollTop() > $("#content-main").offset().top - $("#navbar nav").height()) {
+    if ($(window).scrollTop() + $("#navbar nav").height() > $("#content-main").offset().top - 29) {
+        // If the position of the page in the window (navbar factored in)
+        // is greater than the position of the top of the content-main div,
+        // the user has scrolled past the cover page.
+
+        // `#content-main height - 29` because of an alignment issue where
+        // the cover page would already have been scrolled under the navbar
+        // before the color change occured, causing part of the transparent navbar
+        // to show through onto the content div.
         $("#navbar nav").removeClass("on-cover");
     } else {
         $("#navbar nav").addClass("on-cover");
