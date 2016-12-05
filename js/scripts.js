14961ff66f158879573a840315d6ecb36508fa60
Richie Zhang <richiezhang256@gmail.com>
2016-12-05 00:27:47 -0800

    Make navbar change color when scrolling past the cover page.
    TODO: Fix alignment issues with navbar color change

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 // Main Javascript file
+
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > $("#content-main").offset().top - $("#navbar nav").height()) {
+        $("#navbar nav").removeClass("on-cover");
+    } else {
+        $("#navbar nav").addClass("on-cover");
+    }
+});
