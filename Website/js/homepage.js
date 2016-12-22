49cf1fdb3542c44f6003e15db835de2b670f01db
Richie Zhang <richiezhang256@gmail.com>
2016-12-22 11:01:02 -0800

    Restore navbarCheckDropdown, link to it on the homepage

--- Website/js/homepage.js
+++ Website/js/homepage.js
@@ -9,6 +9,17 @@ function navbarCheckScroll() {
     }
 }
 
+// Make the navbar change color if the dropdown is expanded
+function navbarCheckDropdown() {
+    if (!$("#bs-example-navbar-collapse-1").hasClass("collapsing")) {
+        if ($("#navbar-mobile-toggle").hasClass("collapsed")) {
+            $("#navbar nav").removeClass("on-cover");
+        } else {
+            navbarCheckScroll()
+        }
+    }
+}
+
 $(window).scroll(function() {
     // Do these things onscroll
     navbarCheckScroll();
