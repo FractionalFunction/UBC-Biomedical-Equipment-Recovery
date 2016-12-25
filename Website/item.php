9f5900d957329df1f3c69dd72ccb4f4ece21f01c
Richie Zhang <richiezhang256@gmail.com>
2016-12-24 20:27:08 -0800

    Create item page template

---
+++ Website/item.php
@@ -0,0 +1,24 @@
+<?php
+    include_once 'php/settings.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include_once 'php/html-fragments/html-head.php'; ?>
+
+    <script src="js/subpage.js"></script>
+
+    <title>Item</title>
+</head>
+<body>
+    <?php include_once 'php/html-fragments/navbar.php' ?>
+
+    <div id="content-main">
+		<div class="container content-subpage">
+			
+		</div>
+	</div>
+	<?php include_once 'php/html-fragments/footer.php'; ?>
+</body>
+</html>
