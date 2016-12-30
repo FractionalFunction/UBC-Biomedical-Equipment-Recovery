bfcba8673a76d643252b79efb7a8be36cc5caac6
Richie Zhang <richiezhang256@gmail.com>
2016-12-29 22:40:44 -0800

    Remove example text from footer

--- Website/php/html-fragments/footer.php
+++ Website/php/html-fragments/footer.php
@@ -2,6 +2,5 @@
     <div class="container">
         <h3>The UBC Biomedical Equipment Recovery Project</h3>
         <span>Website developed by Richie Zhang. (<a href="https://github.com/FractionalFunction/UBC-Biomedical-Equipment-Recovery" target="_blank">Source Code</a>)</span>
-        <span>Copyright notices, TOS, Privacy Policy, etc goes here...</span>
     </div>
 </div>
