d27fdd3f22dbe8297e7bc099ff0f76ce7877cd3c
Richie Zhang <richiezhang256@gmail.com>
2017-01-08 11:04:22 -0800

    Create a basic 404 error page

---
+++ Website/errorpages/404.php
@@ -0,0 +1,25 @@
+<?php
+	include_once '../php/settings.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include_once '../php/html-fragments/html-head.php'; ?>
+
+	<script src="/js/subpage.js"></script>
+
+	<title>404: Page Not Found</title>
+</head>
+<body>
+	<?php include_once '../php/html-fragments/navbar.php' ?>
+
+	<div id="content-main">
+		<div class="container content-subpage">
+			<h1>404</h1>
+			<h3>The requested page could not be found.</h3>
+		</div
+	</div>
+	<?php include_once '../php/html-fragments/footer.php'; ?>
+</body>
+</html>
