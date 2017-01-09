ed642e4f92de15b75d2d5f4916fc2cb8240a2e42
Richie Zhang <richiezhang256@gmail.com>
2017-01-08 19:05:42 -0800

    Create error pages 400, 401, 403, and 503

---
+++ Website/errorpages/403.php
@@ -0,0 +1,31 @@
+<?php
+	$transparentNavbar = true;
+	include_once '../php/settings.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include_once '../php/html-fragments/html-head.php'; ?>
+
+	<style>
+		html {
+			height: 100vh;
+			overflow: hidden;
+		}
+	</style>
+
+	<title>403: Forbidden</title>
+</head>
+<body>
+	<?php include_once '../php/html-fragments/navbar.php' ?>
+
+	<div id="content-main">
+		<div class="content-full-height no-bottom-margin vertical-center" style="background-color: #eee;">
+			<div class="container">
+				<h1 style="font-size: 100px;">403</h1>
+				<h2>Access to the requested page was denied.</h2>
+			</div>
+		</div>
+</body>
+</html>
