2fd1899fdfc33f81807430b7df71a83a9e5a7fb9
Richie Zhang <richiezhang256@gmail.com>
2017-03-04 15:04:57 -0800

    Allow different types of items to be shown and hidden.

--- Website/itemlist.php
+++ Website/itemlist.php
@@ -22,35 +22,37 @@
 
 			<div class="row">
 				
-				<h3>Microscopes</h3>
+				<h3>Microscopes <button id="microscopelist-toggle" class="btn btn-primary" onclick="toggleSection('#microscopelist-toggle', '#microscopelist')">Show</button></h3>
 				
-				<?php
-				$sql = $db->query("SELECT * FROM MicroscopeModels;");
-				
-				if ($sql->num_rows > 0) {
-					
-					echo "<div class=\"col-xs-12\"><p>$sql->num_rows Item(s)</p></div><hr />";
-					
-					while($microscopeModel = $sql->fetch_assoc()) {
-						?>
-							<div class="item-thumbnail col-xl-3 col-lg-4 col-md-4 col-sm-6 col-xs-12">
-								<div class="thumbnail">
-									<img src="img/microscopes/<?= $microscopeModel["Image"] ?>" alt="Item Thumbnail">
-									<div class="caption">
-										<h4><?= $microscopeModel["Make"] ?> <?= $microscopeModel["Model"] ?></h4>
-										<p><?= $microscopeModel["Type"] ?></p>
-										<p><?= $microscopeModel["Description"] ?></p>
-										<p><a href="/item.php?id=<?= $microscopeModel["ID"] ?>&type=microscope" class="btn btn-primary" role="button">More Information</a></p>
+				<div class="toggled-section" id="microscopelist">
+					<?php
+					$sql = $db->query("SELECT * FROM MicroscopeModels;");
+
+					if ($sql->num_rows > 0) {
+
+						echo "<div class=\"col-xs-12\"><p>$sql->num_rows Item(s)</p></div><hr />";
+
+						while($microscopeModel = $sql->fetch_assoc()) {
+							?>
+								<div class="item-thumbnail col-xl-3 col-lg-4 col-md-4 col-sm-6 col-xs-12">
+									<div class="thumbnail">
+										<img src="img/microscopes/<?= $microscopeModel["Image"] ?>" alt="Item Thumbnail">
+										<div class="caption">
+											<h4><?= $microscopeModel["Make"] ?> <?= $microscopeModel["Model"] ?></h4>
+											<p><?= $microscopeModel["Type"] ?></p>
+											<p><?= $microscopeModel["Description"] ?></p>
+											<p><a href="/item.php?id=<?= $microscopeModel["ID"] ?>&type=microscope" class="btn btn-primary" role="button">More Information</a></p>
+										</div>
 									</div>
 								</div>
-							</div>
-						<?php
+							<?php
+						}
+					} else {
+						echo "<p>There are no available items.</p>";
 					}
-				} else {
-					echo "<p>There are no available items.</p>";
-				}
 
-				?>
+					?>
+				</div>
 
 			</div>
 			
@@ -58,60 +60,62 @@
 			
 			<div class="row">
 				
-				<h3>Books</h3>
+				<h3>Books <button id="booklist-toggle" class="btn btn-primary" onclick="toggleSection('#booklist-toggle', '#booklist')">Show</button></h3>
 				
-				<table class="table">
-						<thead class="thead-default">
-							<tr>
-								<th>ID</th>
-								<th>ISBN-13</th>
-								<th>Category</th>
-								<th>Title</th>
-								<th>First Author</th>
-								<th>Publisher</th>
-								<th>Edition</th>
-								<th>Market Price</th>
-								<th>Binding</th>
-								<th>Condition</th>
-								<th>Number of Copies</th>
-								<th>Publication Date</th>
-								<th>Notes</th>
-							</tr>
-						</thead>
-						<tbody>
-				
-							<?php
-							$sql = $db->query("SELECT * FROM Books WHERE Visibility = 1;");
+				<div class="toggled-section" id="booklist">
+					<table class="table">
+							<thead class="thead-default">
+								<tr>
+									<th>ID</th>
+									<th>ISBN-13</th>
+									<th>Category</th>
+									<th>Title</th>
+									<th>First Author</th>
+									<th>Publisher</th>
+									<th>Edition</th>
+									<th>Market Price</th>
+									<th>Binding</th>
+									<th>Condition</th>
+									<th>Number of Copies</th>
+									<th>Publication Date</th>
+									<th>Notes</th>
+								</tr>
+							</thead>
+							<tbody>
 
-							if ($sql->num_rows > 0) {
+								<?php
+								$sql = $db->query("SELECT * FROM Books WHERE Visibility = 1;");
 
-								echo "<div class=\"col-xs-12\"><p>$sql->num_rows Item(s)</p></div><hr />";
+								if ($sql->num_rows > 0) {
 
-								while($book = $sql->fetch_assoc()) {
-									?>
-									<tr>
-										<td><?= $book["ID"] ?></td>
-										<td><?= $book["ISBN13"] ?></td>
-										<td><?= $book["Category"] ?></td>
-										<td><?= $book["Title"] ?></td>
-										<td><?= $book["Publisher"] ?></td>
-										<td><?= $book["Edition"] ?></td>
-										<td><?= $book["MarketPrice"] ?></td>
-										<td><?= $book["Binding"] ?></td>
-										<td><?= $book["Condition"] ?></td>
-										<td><?= $book["Copies"] ?></td>
-										<td><?= $book["PublicationDate"] ?></td>
-										<td><?= $book["Notes"] ?></td>
-									</tr>
-									<?php
+									echo "<div class=\"col-xs-12\"><p>$sql->num_rows Item(s)</p></div><hr />";
+
+									while($book = $sql->fetch_assoc()) {
+										?>
+										<tr>
+											<td><?= $book["ID"] ?></td>
+											<td><?= $book["ISBN13"] ?></td>
+											<td><?= $book["Category"] ?></td>
+											<td><?= $book["Title"] ?></td>
+											<td><?= $book["Publisher"] ?></td>
+											<td><?= $book["Edition"] ?></td>
+											<td><?= $book["MarketPrice"] ?></td>
+											<td><?= $book["Binding"] ?></td>
+											<td><?= $book["Condition"] ?></td>
+											<td><?= $book["Copies"] ?></td>
+											<td><?= $book["PublicationDate"] ?></td>
+											<td><?= $book["Notes"] ?></td>
+										</tr>
+										<?php
+									}
+								} else {
+									echo "<p>There are no available items.</p>";
 								}
-							} else {
-								echo "<p>There are no available items.</p>";
-							}
 
-							?>
-						</tbody>
-				</table>
+								?>
+							</tbody>
+					</table>
+				</div>
 
 			</div>
 			
--- Website/js/subpage.js
+++ Website/js/subpage.js
@@ -10,3 +10,13 @@ window.onresize = function() {
 	correctNavbarClearence("#content-main", "0px");
 	correctFooterHeight();
 }
+
+function toggleSection(button, section) {
+	if ($(button).html() === "Show") {
+		$(button).html("Hide")
+		$(section).slideDown();
+	} else {
+		$(button).html("Show")
+		$(section).slideUp();
+	}
+}
